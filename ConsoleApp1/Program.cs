10b8ee61568ff326ec243581c9c26e86fec0463c
BloodWod-513 <dayn_2013@mail.ru>
2021-09-16 16:27:34 +0500

    add method for create person array

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -6,7 +6,29 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Write("Enter amount of persons: ");
+            TMan[] persons = CreatePerson(int.Parse(Console.ReadLine()));
+            Console.ReadKey();
+        }
+        private static TMan[] CreatePerson(int size)
+        {
+            TMan[] persons = new TMan[size];
+            for (int i = 0; i < persons.Length; i++)
+            {
+                Console.Write($"Person {i + 1}\n");
+                Console.Write("Enter FIO: ");
+                string[]  aFio = Console.ReadLine().Split(' ');
+
+                Console.Write("Enter age: ");
+                int age = int.Parse(Console.ReadLine());
+
+                Console.Write("Enter address: ");
+                string address = Console.ReadLine();
+                Console.WriteLine();
+
+                persons[i] = new TMan(aFio[0], aFio[1], aFio[2], age, address);
+            }
+            return persons;
         }
     }
 }
